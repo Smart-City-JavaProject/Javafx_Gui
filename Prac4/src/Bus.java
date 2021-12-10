5122bf901c10a49e2b644bc2b8bc4df57bd073e3
YourDad07 <salmanadhikari415@gmail.com>
2021-12-10 15:30:54 +0530

    Fix The Typo

--- Prac4/src/Bus.java
+++ Prac4/src/Bus.java
@@ -1,13 +1,9 @@
 import java.io.IOException;
 import java.sql.SQLException;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.ImageView;
