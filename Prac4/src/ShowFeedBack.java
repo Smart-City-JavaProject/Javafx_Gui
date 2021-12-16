04616fce22c0b094defc45764f670c2ba316e1ab
YourDad07 <salmanadhikari415@gmail.com>
2021-12-16 19:58:39 +0530

    Improve The Ui

--- Prac4/src/ShowFeedBack.java
+++ Prac4/src/ShowFeedBack.java
@@ -3,6 +3,7 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
@@ -10,6 +11,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
@@ -70,33 +72,63 @@ public class ShowFeedBack implements Initializable {
                 System.out.println("---->>> " + feedbacks[i]);
                 label.setWrapText(true);
                 label.setPrefWidth(1500);
-                label.setStyle(
-                        "-fx-font-size: 20px; -fx-font-weight: bold; -fx-text-fill: #000000;-fx-font-family: \"Segoe UI\";");
-                label.setTextAlignment(TextAlignment.JUSTIFY);
+                if (i % 2 != 0) {
+                    label.setStyle(
+                            "-fx-font-size: 20px; -fx-font-weight: bold; -fx-text-fill: #ffffff;-fx-font-family: \"Segoe UI\";");
+                    label.setTextAlignment(TextAlignment.JUSTIFY);
 
-                name_label.setStyle(
-                        "-fx-font-size: 20px; -fx-font-weight: bold; -fx-text-fill: #000000; -fx-font-family: \"Segoe UI\";");
-                name_label.setTextAlignment(TextAlignment.CENTER);
+                    name_label.setStyle(
+                            "-fx-font-size: 20px; -fx-font-weight: bold; -fx-text-fill: #ffffff; -fx-font-family: \"Segoe UI\";");
+                    name_label.setTextAlignment(TextAlignment.CENTER);
+                } else {
+
+                    label.setStyle(
+                            "-fx-font-size: 20px; -fx-font-weight: bold; -fx-text-fill: #000000;-fx-font-family: \"Segoe UI\";");
+                    label.setTextAlignment(TextAlignment.JUSTIFY);
+
+                    name_label.setStyle(
+                            "-fx-font-size: 20px; -fx-font-weight: bold; -fx-text-fill: #000000; -fx-font-family: \"Segoe UI\";");
+                    name_label.setTextAlignment(TextAlignment.CENTER);
+                }
                 stage.setTitle("Smart City");
                 HBox hbox = new HBox();
                 VBox vbox1 = new VBox();
+                vbox1.setPadding(new Insets(10, 0, 0, 10));
                 Circle cc = new Circle(40);
                 cc.setFill(new ImagePattern(image));
-                cc.setStroke(javafx.scene.paint.Color.BLACK);
+                if (i % 2 != 0) {
+
+                    cc.setStroke(javafx.scene.paint.Color.WHITE);
+                } else {
+
+                    cc.setStroke(javafx.scene.paint.Color.BLACK);
+                }
+                label.setPadding(new Insets(30, 0, 0, 0));
+                name_label.setPadding(new Insets(5, 0, 0, 25));
                 vbox1.getChildren().add(cc);
                 vbox1.getChildren().add(name_label);
+                // vbox1.setMaxHeight(600);
+                // vbox1.setMaxWidth(300);
+                // vbox1.setPadding(new Insets(10, 100, 0, 10));
                 hbox.getChildren().add(vbox1);
                 hbox.getChildren().add(label);
+                if (i % 2 != 0) {
+
+                    hbox.setStyle("-fx-background-color: #000000;");
+                } else {
+                    hbox.setStyle("-fx-background-color: #ffffff;");
+
+                }
                 // hbox.setMaxWidth(200);
                 hbox.setSpacing(100);
                 vbox.getChildren().add(hbox);
-                vbox.setSpacing(40);
+                // vbox.setSpacing(40);
             }
 
         }
         ScrollPane sp = new ScrollPane();
         sp.setContent(vbox);
-        Scene scene = new Scene(sp, 600, 400);
+        Scene scene = new Scene(sp, 880, 500);
         stage.setScene(scene);
         stage.show();
     }
